4d59149b7cda539b48d889123fc186a1172c860c
Soukaina Samira <samira.merrakchi@hotmail.com>
2023-06-06 15:52:17 -0700

    all

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Author - Godswill Kalu
+"""Print the numbers from 1 to 100 separated by a space."""
+
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
