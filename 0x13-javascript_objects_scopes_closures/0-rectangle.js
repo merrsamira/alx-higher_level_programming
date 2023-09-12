1f6ab6a20335dc434d62301c97024a2f7d914d78
merrsamira <125443409+merrsamira@users.noreply.github.com>
2023-09-12 21:49:17 +0100

    sm

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-//empty class Rectangle that defines a rectangle
-class Rectangle {
-}
-
+// class Rectangle that defines a rectangle
+class Rectangle {}
 module.exports = Rectangle;
